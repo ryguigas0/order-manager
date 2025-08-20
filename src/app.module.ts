e0c3c4d0e42d8856a999bdfb1d37d64777c2ab45
Guilherme Oliveira Casagrande <32659090+ryguigas0@users.noreply.github.com>
2025-08-20 14:49:45 -0300

    added test rabbitmq connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CATS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:password@localhost:5672'],
+          queue: 'notifications',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
